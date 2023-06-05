cd228c9fb124e80832c00663702165c2c04d17fe
Facundo Lucero <facundon.lucero@alumnos.frm.utn.edu.ar>
2023-06-05 20:58:19 -0300

    ssh keys fix

--- home/gnome.nix
+++ home/gnome.nix
@@ -2,12 +2,12 @@
 
 
 {
-
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   programs.dconf.enable = true;
 
   environment.systemPackages = with pkgs; [
+    gnome.gnome-keyring
     gnome.gnome-tweaks
     gnomeExtensions.appindicator
     gnomeExtensions.tray-icons-reloaded
